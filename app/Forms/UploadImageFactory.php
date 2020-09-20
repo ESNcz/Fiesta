96313c1dbc34d2866abdcb480e223a347e659d6b
Josef Kolář <thejoeejoee@gmail.com>
2020-09-20 20:32:00 +0200

    Refactored duplicated upload form creation

--- app/Forms/UploadImageFactory.php
+++ app/Forms/UploadImageFactory.php
@@ -16,8 +16,8 @@ class UploadImageFactory
     /**
      * SignUpFormFactory constructor.
      * @param DefaultFormRenderer $renderer
-     * @param UserRepository $user
-     * @param Translator $translator
+     * @param UserRepository      $user
+     * @param Translator          $translator
      */
     public function __construct(DefaultFormRenderer $renderer,
                                 UserRepository $user,
@@ -37,48 +37,36 @@ class UploadImageFactory
      */
     public function uploadUserImage(callable $onSuccess)
     {
-
-        $form = $this->renderer->create();
-        $form->getElementPrototype()->class('ajax');
-        $form->addUpload('upload', '', FALSE)
-            ->setRequired(false)
-            ->addRule(Form::IMAGE, "The uploaded file must be image in format JPEG or PNG.", array('image/jpeg', 'image/png'))
-            ->addRule(Form::MAX_FILE_SIZE, "The size of the uploaded image cannot be up to 2 MB", 2000000 /* v bytech */)
-            ->setHtmlAttribute('class', "hidden");
-
-        $form->addSubmit('send', 'Send')
-            ->setHtmlAttribute('id', "sendUpload")
-            ->setHtmlAttribute("class", "hidden");
-        /**
-         * @param Form $form
-         * @param      $values
-         */
-        $form->onSuccess[] = function (Form $form, $values) use ($onSuccess) {
-            if ($values->upload->isImage() and $values->upload->isOk()) {
-                $image = Image::fromFile($values->upload);
-                $image->resize(300, 300, Image::EXACT);
-                $image->save("images/avatar/{$this->user->identity->signature}.jpg", 100, Image::JPEG);
-            }
-            $onSuccess();
-        };
-
-        return $form;
+        return $this->createBaseUploadForm(
+            $this->user->identity->signature,
+            $onSuccess
+        );
     }
 
     /**
      * Upload Image to server (anyone)
-     * @param $signature
+     * @param          $signature
      * @param callable $onSuccess
      * @return Form
      */
     public function uploadImage($signature, callable $onSuccess)
     {
+        return $this->createBaseUploadForm(
+            $signature,
+            $onSuccess
+        );
+    }
 
+    /**
+     * Creates base for upload form (file input & submit button)
+     * @param          $userSignature
+     * @param callable $onSuccess
+     * @return Form
+     */
+    protected function createBaseUploadForm($userSignature, callable $onSuccess)
+    {
         $form = $this->renderer->create();
         $form->getElementPrototype()->class('ajax');
-
-        $form->addHidden("signature")->setDefaultValue($signature);
-
         $form->addUpload('upload', '', FALSE)
             ->setRequired(false)
             ->addRule(Form::IMAGE, "The uploaded file must be image in format JPEG or PNG.", array('image/jpeg', 'image/png'))
@@ -88,6 +76,9 @@ class UploadImageFactory
         $form->addSubmit('send', 'Send')
             ->setHtmlAttribute('id', "sendUpload")
             ->setHtmlAttribute("class", "hidden");
+
+        $form->addHidden("signature")->setDefaultValue($userSignature);
+
         /**
          * @param Form $form
          * @param      $values
